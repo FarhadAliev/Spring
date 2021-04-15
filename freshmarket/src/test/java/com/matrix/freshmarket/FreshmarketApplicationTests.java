fca6143d44e8bca7824b201d1ada7a2a4c8c57df
Farhad <77681628+Aliyev1993@users.noreply.github.com>
2021-04-15 13:32:28 +0400

    15.04.2021

---
+++ freshmarket/src/test/java/com/matrix/freshmarket/FreshmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix.freshmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
