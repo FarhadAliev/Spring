3474f6cf3b1f28602abeb3f53e68c24bf0eec3a9
Farhad <77681628+Aliyev1993@users.noreply.github.com>
2021-04-15 17:54:02 +0400

    15.04.2021

---
+++ freshmarket/src/main/java/com/matrix/freshmarket/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.matrix.freshmarket.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String homePage( Model model) {
+        model.addAttribute("title", "home");
+        return "FreshMarket";
+    }
+}
